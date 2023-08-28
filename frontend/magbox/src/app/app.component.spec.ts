4746ee72b3d03c7a42c60f099931fa1e2ed6a142
TimHi <timhiller@live.de>
2023-08-29 00:06:57 +0200

    remove unused test

--- frontend/magbox/src/app/app.component.spec.ts
+++ frontend/magbox/src/app/app.component.spec.ts
@@ -25,11 +25,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('magbox');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('magbox app is running!');
-  });
 });
