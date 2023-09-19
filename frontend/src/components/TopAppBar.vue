9a811611b584aa90c68d0b8a706000b41f695d26
TimHi <timhiller@live.de>
2023-09-19 12:40:36 +0200

    build containers

--- frontend/src/components/TopAppBar.vue
+++ frontend/src/components/TopAppBar.vue
@@ -1,6 +1,31 @@
+<script setup lang="ts">
+import router from '../router';
+import { PocketBaseService } from '../service/pocketBaseService';
+import { useUserStore } from '../stores/user';
+const pb = new PocketBaseService();
+const user = useUserStore();
+
+
+function login() {
+    if (user.user.isLoggedIn) {
+        pb.Logout();
+    } else {
+        try {
+            pb.SignInUsingOAuth2().then(() => router.push('/')).catch(() => console.error("Login Error"));
+        } catch (err) { console.error(err) }
+    }
+}
+</script>
+
 <template>
     <div class="topAppBar">
         <h1>Magbox</h1>
+    </div>
+    <div>
+        <el-button type="primary" @click="login">
+            <p v-if="user.user.isLoggedIn">Logout</p>
+            <p v-else>Login</p>
+        </el-button>
     </div>
 </template>
 
--- frontend/src/views/LoginPage.vue
+++ frontend/src/views/LoginPage.vue
@@ -1,25 +1,3 @@
-<script setup lang="ts">
-import router from '../router';
-import { PocketBaseService } from '../service/pocketBaseService';
-import { useUserStore } from '../stores/user';
-const pb = new PocketBaseService();
-const user = useUserStore();
-
-
-function login() {
-    if (user.user.isLoggedIn) {
-        pb.Logout();
-    } else {
-        try {
-            pb.SignInUsingOAuth2().then(() => router.push('/')).catch(() => console.error("Login Error"));
-        } catch (err) { console.error(err) }
-    }
-}
-</script>
 <template>
-    <h1>Login to your Box</h1>
-    <el-button type="primary" @click="login">
-        <p v-if="user.user.isLoggedIn">Logout</p>
-        <p v-else>Login</p>
-    </el-button>
+    <TopAppBar />
 </template>
