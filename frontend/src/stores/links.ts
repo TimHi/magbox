bda6636d6574334b9e639e61f48d62d0f9d5bda7
TimHi <TimHiller@live.de>
2023-09-23 13:20:18 +0200

    fix npm build

--- frontend/src/stores/links.ts
+++ frontend/src/stores/links.ts
@@ -3,34 +3,33 @@
 import { defineStore } from 'pinia';
 import type { LinkModel } from '../model/linkModel';
 import { PocketBaseService } from '../service/pocketBaseService';
-import { DocumentPreview } from '../model/previewModel';
+import type { DocumentPreview } from '../model/previewModel';
 
 const pbService = new PocketBaseService();
 export const useLinkStore = defineStore('linkStore', {
-    state: () => ({
-        links: [] as LinkModel[],
-    }),
-    actions: {
-        async getLinksFromBackend() {
-            this.links = await pbService.GetLinks() as LinkModel[];
-            console.log("Store updated");
-        },
-        async addLink(link: string, preview: DocumentPreview | undefined, read = false) {
-            const result = await pbService.CreateLink(link, preview, read);
-            if (result) {
-                this.links.push(result);
-            }
-        },
-        async removeLink(id: string) {
-            const result = await pbService.DeleteLinkEntry(id);
-            if (result) {
-                this.links = this.links.filter((v) => v.id !== id);
-            }
-        },
+  state: () => ({
+    links: [] as LinkModel[]
+  }),
+  actions: {
+    async getLinksFromBackend() {
+      this.links = (await pbService.GetLinks()) as LinkModel[];
     },
-    getters: {
-        getAllLinks(): LinkModel[] {
-            return this.links;
-        },
+    async addLink(link: string, preview: DocumentPreview | undefined, read = false) {
+      const result = await pbService.CreateLink(link, preview, read);
+      if (result) {
+        this.links.push(result);
+      }
     },
+    async removeLink(id: string) {
+      const result = await pbService.DeleteLinkEntry(id);
+      if (result) {
+        this.links = this.links.filter((v) => v.id !== id);
+      }
+    }
+  },
+  getters: {
+    getAllLinks(): LinkModel[] {
+      return this.links;
+    }
+  }
 });
