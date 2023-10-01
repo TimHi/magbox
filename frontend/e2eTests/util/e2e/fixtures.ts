8c1a9865cf5bd0654a1f85570c75ae49490cf3f4
TimHi <timhiller@live.de>
2023-10-01 21:08:10 +0200

    e2e tests for tags

--- frontend/e2eTests/util/e2e/fixtures.ts
+++ frontend/e2eTests/util/e2e/fixtures.ts
@@ -5,8 +5,11 @@ import logindata from '../MockData/login.json';
 import linkData from '../MockData/linkData.json';
 import postNewLink from '../MockData/postNewLink.json';
 import githubPreviewData from '../MockData/githubPreview.json';
+import postTag from '../MockData/addNewTagData.json';
+import emptyTags from "../MockData/emptyTags.json";
 
 base.beforeEach(async ({ page }) => {
+
   await page.route('*/**/api/collections/users/auth-methods', async (route) => {
     await route.fulfill({ body: JSON.stringify(logindata), contentType: 'application/json' });
   });
@@ -28,6 +31,21 @@ base.beforeEach(async ({ page }) => {
     '*/**/api/collections/links/records',
     async (route) => {
       await route.fulfill({ body: JSON.stringify(postNewLink), contentType: 'application/json' });
+    }
+  );
+
+  await page.route(
+    '*/**/api/collections/categories/records?page=1&perPage=500&skipTotal=1',
+    async (route) => {
+      await route.fulfill({ body: JSON.stringify(emptyTags), contentType: 'application/json' });
+    }
+  );
+
+  await page.route(
+    '*/**/api/collections/categories/records',
+    async (route) => {
+      console.log("abgeaalt");
+      await route.fulfill({ body: JSON.stringify(postTag), contentType: 'application/json' });
     }
   );
 });
