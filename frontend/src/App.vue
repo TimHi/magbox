50c9c1e875ad9fdca312a597b1f7d309697cb3cd
TimHi <TimHiller@live.de>
2023-10-26 00:52:48 +0200

    move footer to bottom

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,10 +3,10 @@ import FooterBar from './components/FooterBar.vue';
 </script>
 
 <template>
-  <router-view>
-
-  </router-view>
-  <FooterBar class="margins"></FooterBar>
+  <router-view></router-view>
+  <div id="footer">
+    <FooterBar class="margins" />
+  </div>
 </template>
  
 <style>
