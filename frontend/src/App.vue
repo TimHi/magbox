2a6b1e49a5bcfc5cfe3ea1a8e891087c413bdc01
TimHi <TimHiller@live.de>
2024-01-19 23:42:37 +0100

    fix footer overlap

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,13 +3,22 @@ import FooterBar from './components/FooterBar.vue';
 </script>
 
 <template>
-  <router-view></router-view>
-  <div id="footer">
-    <FooterBar class="margins" />
+  <div>
+    <div class="content">
+      <router-view></router-view>
+    </div>
+
+    <div id="footer">
+      <FooterBar class="margins" />
+    </div>
   </div>
 </template>
- 
+
 <style>
+.content {
+  padding-bottom: 24px;
+}
+
 .margins {
   margin-left: 100px;
   margin-bottom: 4px;
