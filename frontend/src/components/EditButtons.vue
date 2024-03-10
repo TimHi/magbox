8894ce53ce7fb964eaa31234b776c21b2df7d4bd
TimHi <TimHiller@live.de>
2024-03-10 01:01:48 +0100

    make cards prettier

--- frontend/src/components/EditButtons.vue
+++ frontend/src/components/EditButtons.vue
@@ -2,41 +2,58 @@
 import type { PropType } from 'vue';
 import type { LinkModel } from '../model/linkModel';
 
-
 defineProps({
-    linkModel: Object as PropType<LinkModel>,
-    isEditMode: Boolean,
-    saveChanges: Function,
-    cancelEditing: Function,
-    deleteItem: Function,
-    editItem: Function
+  linkModel: Object as PropType<LinkModel>,
+  isEditMode: Boolean,
+  saveChanges: Function,
+  cancelEditing: Function,
+  deleteItem: Function,
+  editItem: Function
 });
-
 </script>
 <template>
-    <div class="edit-save-buttons">
-        <el-icon v-if="!isEditMode" class="icon" color="white" @click="editItem" data-testid="btn-edit-item">
-            <Edit />
-        </el-icon>
-        <el-icon v-if="isEditMode" class="icon" color="white" @click="cancelEditing" data-testid="btn-cancel-edit">
-            <Close />
-        </el-icon>
-        <el-icon v-if="isEditMode" class="icon" color="white" @click="saveChanges" data-testid="btn-save-changes">
-            <Check />
-        </el-icon>
-        <el-icon color="white" class="icon" @click="deleteItem" data-testid="btn-delete-item">
-            <Delete />
-        </el-icon>
-    </div>
+  <div class="edit-save-buttons">
+    <el-icon
+      v-if="!isEditMode"
+      class="icon"
+      color="white"
+      @click="editItem"
+      data-testid="btn-edit-item"
+    >
+      <Edit />
+    </el-icon>
+    <el-icon
+      v-if="isEditMode"
+      class="icon"
+      color="white"
+      @click="cancelEditing"
+      data-testid="btn-cancel-edit"
+    >
+      <Close />
+    </el-icon>
+    <el-icon
+      v-if="isEditMode"
+      class="icon"
+      color="white"
+      @click="saveChanges"
+      data-testid="btn-save-changes"
+    >
+      <Check />
+    </el-icon>
+    <el-icon color="white" class="icon" @click="deleteItem" data-testid="btn-delete-item">
+      <Delete />
+    </el-icon>
+  </div>
 </template>
 <style>
 .icon {
-    cursor: pointer;
-    margin: 4px;
+  cursor: pointer;
+  margin: 4px;
 }
 
 .edit-save-buttons {
-    display: flex;
-    flex-direction: row;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
 }
 </style>
