1475abc18f6c31cf4070c47dac1ef4174a793871
TimHi <TimHiller@live.de>
2025-02-09 19:40:37 +0100

    ....

--- backend/main.go
+++ backend/main.go
@@ -5,8 +5,6 @@ import (
 
 	"github.com/badoux/goscraper"
 	"github.com/charmbracelet/log"
-	"github.com/labstack/echo/v5"
-	"github.com/labstack/echo/v5/middleware"
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
@@ -18,27 +16,22 @@ func main() {
 	// go run .\main.go serve --http="localhost:9000"
 	// go run main.go serve --http="localhost:9000"
 	// serves static files from the provided public dir (if exists)
-	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
-		e.Router.AddRoute(echo.Route{
-			Method: http.MethodGet,
-			Path:   "/api/url_preview/:url",
-			Handler: func(c echo.Context) error {
-				url := c.PathParam("url")
-				s, err := goscraper.Scrape(url, 3)
-				if err != nil {
-					log.Print(err)
-					return c.JSON(http.StatusInternalServerError, "Error Scraping")
-				}
-				return c.JSON(http.StatusOK, s.Preview)
-			},
-			Middlewares: []echo.MiddlewareFunc{
-				apis.ActivityLogger(app),
-				middleware.CORSWithConfig(middleware.CORSConfig{
-					AllowOrigins: []string{"*"},
-				}),
-			},
+
+	app.OnServe().BindFunc(func(se *core.ServeEvent) error {
+		// register "GET /hello/{name}" route (allowed for everyone)
+		se.Router.GET("/api/url_preview/:url", func(e *core.RequestEvent) error {
+			url := e.Request.PathValue("url")
+			s, err := goscraper.Scrape(url, 3)
+			if err != nil {
+				log.Print(err)
+				return e.JSON(http.StatusInternalServerError, "Error Scraping")
+			}
+			return e.JSON(http.StatusOK, s.Preview)
 		})
-		return nil
+		apis.CORS(apis.CORSConfig{
+			AllowOrigins: []string{"*"},
+		})
+		return se.Next()
 	})
 
 	if err := app.Start(); err != nil {
