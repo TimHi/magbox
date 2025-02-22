7baf966f56208b007e5a60b37afad805f30d1819
TimHi <TimHiller@live.de>
2025-02-22 14:31:42 +0100

    stuff

--- frontend/src/views/GridPage.vue
+++ frontend/src/views/GridPage.vue
@@ -1,14 +1,19 @@
 <script setup lang="ts">
-import HomePage from './HomePage.vue';
+import { ref } from 'vue';
+import { LinkModel } from '../model/linkModel';
+import { useLinkStore } from '../stores/links';
+import { useTagStore } from '../stores/tags';
+
+
+
+const linkStore = useLinkStore();
+
+let linksInStore = ref<Array<LinkModel>>(linkStore.getAllLinks);
 </script>
 
 <template>
-  <div class="bg-black">
-    <h1 class="bg-green">AAL</h1>
-    <h1 class="gold">Gold</h1>
-    <h1 class="text-3xl font-bold underline text-gold">Hello world!</h1>
-  </div>
-  <div class="card flex justify-center">
-    <PButton class="bg-red" label="Verify 2" />
-  </div>
+ <PCard>
+  
+ </PCard>
+ 
 </template>
