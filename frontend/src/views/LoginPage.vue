7c8569a3b20bed00dc7c1405b5a616dee7831c65
TimHi <TimHiller@live.de>
2025-02-27 18:43:16 +0100

    adding primevue and tailwind support
    - updated tsconfig as it is increate-vue example
    - adding tailwindv4
    - adding primevue

--- frontend/src/views/LoginPage.vue
+++ frontend/src/views/LoginPage.vue
@@ -6,41 +6,41 @@ const user = useUserStore();
 const pb = new PocketBaseService();
 
 function login() {
-    if (user.user.isLoggedIn) {
-        pb.Logout();
-    } else {
-        try {
-            pb.SignInUsingOAuth2().catch(() => console.error("Login Error"));
-        } catch (err) { console.error(err) }
+  if (user.user.isLoggedIn) {
+    pb.Logout();
+  } else {
+    try {
+      pb.SignInUsingOAuth2().catch(() => console.error('Login Error'));
+    } catch (err) {
+      console.error(err);
     }
+  }
 }
 </script>
 
 <template>
-    <TopAppBar />
-    <div class="descBox">
-        <div>
-            <h2>Keep track of stuff you want to read later 📔✍️</h2>
-            <h2>Sort, categorize and filter (WIP)📑</h2>
-            <h2>Export to Obsidian (WIP) 📤💎</h2>
-            <h2>Send links directly to your using the browser extension (WIP) 📬</h2>
-        </div>
-        <div>
-            <el-button data-testid="login-button" type="primary" @click="login">
-                <p v-if="!user.user.isLoggedIn">Login</p>
-            </el-button>
-        </div>
+  <TopAppBar />
+  <div class="descBox">
+    <div>
+      <h2>Keep track of stuff you want to read later 📔✍️</h2>
+      <h2>Sort, categorize and filter (WIP)📑</h2>
+      <h2>Export to Obsidian (WIP) 📤💎</h2>
+      <h2>Send links directly to your using the browser extension (WIP) 📬</h2>
     </div>
+    <div>
+      <Button data-testid="login-button" severity="primary" label="Login" @click="login"></Button>
+    </div>
+  </div>
 </template>
 
 <style>
 .descBox {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 
 .mag {
-    color: var(--color-magbox-blue);
+  color: var(--color-magbox-blue);
 }
 </style>
--- frontend/vite.config.mts
+++ frontend/vite.config.mts
@@ -3,6 +3,9 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import Components from 'unplugin-vue-components/vite';
+import { PrimeVueResolver } from '@primevue/auto-import-resolver';
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +13,12 @@ export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    tailwindcss(),
+    Components({
+      resolvers: [
+        PrimeVueResolver()
+      ]
+    })
   ],
   define: {
     APP_VERSION: JSON.stringify(process.env.npm_package_version),
