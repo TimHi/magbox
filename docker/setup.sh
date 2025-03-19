5b55ac525d0eb1c50307861dd69ede81ed7fcbe8
TimHi <TimHiller@live.de>
2025-03-19 01:31:55 +0100

    setup docker stuff for e2e tests

--- docker/setup.sh
+++ docker/setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 cd ../frontend
-docker build -t magbox-frontend .
+docker build -t magbox-frontend  --build-arg VITE_ENV=development .
 
 cd ../backend
 docker build -t magbox-backend .
 
 cd ../docker
-docker compose up -d
+$env:MODE="development"; docker-compose up --build -d
 
 cd ../e2e
 docker run --rm --name playwright-tests --network magbox-network --env CI=true --entrypoint npx
