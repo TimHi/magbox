bc6f1495fbb01583376d9e4539158e82cafa0226
TimHi <TimHiller@live.de>
2025-03-23 01:11:01 +0100

    install with dev dependencies
    @vitejs/plugin-vue went missing

--- docker/setup.sh
+++ docker/setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+#1 - true/false for e2e
+#2 - build or leave empty
 cd ../frontend
 echo "Build frontend"
 docker build -t magbox-frontend --build-arg VITE_ENV=test .
@@ -11,12 +12,13 @@ docker build -t magbox-backend .
 cd ../docker
 echo "Starting Container"
 if [ "$OS" = "Windows_NT" ]; then
-  $env:MODE="test"; docker-compose up --build -d
+  $env:MODE="test"; docker-compose up $2 -d
 else
-  MODE="test" docker-compose up --build -d
+  MODE="test" docker-compose up $2 -d
 fi
 
-if [ "$1" = "e2e" ]; then
+if [ "$1" = "true" ]; then
+  cd ../e2e
   echo "Starting e2e Tests..."
   docker run --rm --name playwright-tests --network docker_magbox-network --env CI=true -v "${PWD}:/workspace" -w /workspace --entrypoint npx mcr.microsoft.com/playwright:v1.51.1-jammy playwright test
 fi
